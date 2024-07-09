e89471d3f982ff2b26b4806b6799acb738bf999e
DylanJoo <dylanjootw@gmail.com>
2024-07-09 10:57:09 +0200

    add prompts

--- tools/prepare_gpt_rewrite.py
+++ tools/prepare_gpt_rewrite.py
@@ -1,49 +1,37 @@
+from tqdm import tqdm
 import argparse
 import json
 
+template = """
+Generate a search query for the given user request. The user's background is also provided (some information might help customize a more engaging search query). The query will serve as keywords and pass through a news passage searching engine. The query should be comprehensive and clearly indicate the information needs.\n\n### User request: {REQUEST}\n\n### User background: {BACKGROUND}\n\n### Query:""".strip()
 
-promtps = """
-Generate a searching query for the given request from an user. The query will serve as a keyword pair to the search engine.
-The user's background is also provided as reference. The query should be able to reflect comprehensive information need from the request itself and also the user background.
-""".strip()
+def prepare(args):
 
-promtps = """
-Generate a search query based the given request from the user. The user's background is also provided for the reference.
-""".strip()
+    writer = open(args.output_csv, 'w') 
 
-def main(args):
-
+    eval_data = []
     with open(args.input_jsonl, 'r') as file:
         for line in tqdm(file):
             item = json.loads(line.strip())
-            item['requrest_id'] = 
-            item['collection_ids'] = 
-            background = item['background'] 
-            problem = item['problem_statement'] 
-            item['limit'] = 
+            eval_data.append(item)
+            # request_id = item['requrest_id']
+            # collection_ids = item['collection_ids']
+            # background = item['background'] 
+            # problem = item['problem_statement'] 
+            # limit = item['limit']
 
-    for data in output['data'][:30]:
-        print("===== Q:", data['question'].strip(), "=====")
-        print("A:", data['answer'].strip())
+    for i, eval_item in enumerate(eval_data):
+        id = str(i)
+        prompt = template.replace("{REQUEST}", eval_item['problem_statement'])
+        prompt = prompt.replace("{BACKGROUND}", eval_item['background'])
+        writer.write(f"{id}, {prompt}\n")
 
-        claims = ""
-        for i, claim in enumerate(data['claims']):
-            claims += f"C-{i+1}: {claim.strip()}\n"
-        print(claims)
-
-        print("O:", data['output'].strip())
-
-        docs = ""
-        for i, doc in enumerate(data['docs']):
-            docs += f"D-{i+1}: {doc['title'].strip()}\n"
-        if docs == "":
-            print('D-0: NO Documents')
-        else:
-            print(docs)
+    writer.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Print output")
     parser.add_argument("-input", "--input_jsonl", type=str, default=None)
     parser.add_argument("-output", "--output_csv", type=str, default=None)
     args = parser.parse_args()
-    main(args)
+
+    prepare(args)
