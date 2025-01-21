bd08fe8cb52c0c0bc4a652286eaff841e7b3feaa
DylanJoo <dylanjootw@gmail.com>
2025-01-21 15:21:24 +0100

    [listwise] adopt rank llm into repo

--- src/augment/listwise/reranking/__main__.py
+++ src/augment/listwise/reranking/__main__.py
@@ -1,103 +1,94 @@
-""" 
-[TODO] document segmentation and aggregation
-[TODO] think about long-document scenario. Chunk-and-aggregate?
 """
+The listwise ranking is mostly relying on `rank_llm` repositary.
+See details in `https://github.com/castorini/rank_llm`
+"""
+import copy
+from typing import Any, Dict, List, Union
 
-from operator import itemgetter
-import os
-import datetime
-import logging
-import argparse 
-from tqdm import tqdm
-import json
-import torch
+from rank_llm.data import Query, Request, Candidate
+from rank_llm.rerank import IdentityReranker, RankLLM, Reranker, PromptMode
+from rank_llm.rerank.reranker import extract_kwargs
 
-from tools.ranking_utils import (
-    load_runs, 
-    load_corpus,
-    load_topics, 
-    batch_iterator
-)
-from augment.pointwise.reranking.utils import load_reranker
+
+def convert_runs_to_requests(topics=None, corpus=None, runs=None, top_k=50):
+    requests = []
+    for qid in runs:
+
+        candidates = []
+        top_k_runs = runs[qid]
+        for i, docid in enumerate(runs[qid]):
+            if i < top_k:
+                candidates.append(
+                    Candidate(docid=docid, score=runs[qid][docid], doc=corpus[docid])
+                )
+
+        requests.append(
+                Request(
+                    query=Query(text=topics[qid], qid=qid),
+                    candidates=candidates
+                )
+        )
+    return requests
 
 def rerank(
-    topics, corpus, runs,
-    reranker_config,
-    top_k, batch_size,
-    max_length,
-    writer=None
+    topics, corpus, runs, 
+    model_path: str,
+    top_k: int = 100,
+    shuffle_candidates: bool = False,
+    print_prompts_responses: bool = False,
+    num_passes: int = 1,
+    interactive: bool = False,
+    default_agent: RankLLM = None,
+    **kwargs: Any,
 ):
 
-    reranker = load_reranker(**reranker_config)
+    # Adopt arguemnts to RankLLM
+    kwargs['prompt_mode'] = PromptMode.RANK_GPT if kwargs['prompt_mode'] == 'rank_GPT' else None
 
-    qids = list(topics.keys())
-    qids = [qid for qid in qids if qid in runs]  # only appeared in run
+    # Get reranking agent
+    reranker = Reranker(
+        Reranker.create_agent(model_path, default_agent, interactive, **kwargs)
+    )
 
+    # Transform the retrieval runs into requests
+    requests = convert_runs_to_requests(
+        topics=topics, corpus=corpus, runs=runs, top_k=top_k, 
+    )
+
+    # Reranking stages
+    print(f"Reranking and returning {top_k} passages with {model_path}...")
+    for pass_ct in range(num_passes):
+        print(f"Pass {pass_ct + 1} of {num_passes}:")
+
+        rerank_results = reranker.rerank_batch(
+            requests,
+            rank_end=top_k,
+            rank_start=0,
+            shuffle_candidates=shuffle_candidates,
+            logging=print_prompts_responses,
+            top_k_retrieve=top_k,
+            vllm_batched=kwargs.pop('vllm_batched'),
+            batch_size=kwargs.pop('batch_size'),
+            **kwargs,
+        )
+
+    if num_passes > 1:
+        requests = [
+            Request(copy.deepcopy(r.query), copy.deepcopy(r.candidates))
+            for r in rerank_results
+        ]
+
+    # final postprocessing
     outputs = {}
-    for qid in tqdm(qids, total=len(qids)):
+    for rr in rerank_results:
+        rr.candidates = rr.candidates[:top_k]
 
-        result = runs[qid]
-        query = topics[qid]
-        documents = [corpus[docid] for docid in result]
-
-        # predict
-        scores = []
-        for batch_docs in batch_iterator(documents, batch_size):
-            queries = [query] * len(batch_docs)
-            batch_scores = reranker.predict(
-                queries=queries,
-                documents=[doc['text'] for doc in batch_docs],
-                titles=[doc['title'] for doc in batch_docs],
-                max_length=max_length
-            )
-            scores.extend(batch_scores)
-
-        # sort candidates
-        hits = {docid: scores[i] for i, docid in enumerate(result)}            
-        sorted_result = {k: v for k,v in sorted(hits.items(), key=itemgetter(1), reverse=True)} 
-        outputs[qid] = sorted_result
-
-        # write
-        if writer is not None:
-            for i, (docid, score) in enumerate(sorted_result.items()):
-                writer.write(f"{qid} Q0 {docid} {str(i+1)} {score} {reranker}\n")
+        query = rr.query
+        candidates = rr.candidates
+        outputs[query.qid] = {c.docid: c.score for c in candidates}
 
     return outputs
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--topic_file", type=str, default=None)
-    parser.add_argument("--corpus_dir_or_file", type=str, default=None)
-    parser.add_argument("--run_file", type=str, default=None)
-
-    parser.add_argument("--top_k", type=int, default=1000)
-    parser.add_argument("-bs", "--batch_size_per_query", type=int, default=2)
-    parser.add_argument("--max_length", type=int, default=384)
-    parser.add_argument("--output", type=str, default=None)
-
-    # reranker config
-    parser.add_argument("--reranker_class", type=str, default=None)
-    parser.add_argument("--reranker_name_or_path", type=str, default=None)
-    parser.add_argument("--device", type=str, default='cpu')
-    parser.add_argument("--fp16", default=False, action='store_true')
-    args = parser.parse_args()
-
-    os.makedirs(args.output.rsplit('/', 1)[0], exist_ok=True)
-    writer = open(args.output, 'w')
-
-    ## load data
-    topics = load_topics(args.topic_file)
-    corpus = load_corpus(args.corpus_dir_or_file)
-    runs = load_runs(args.run_file, topk=args.top_k, output_score=True)
-
-    with torch.no_grad():
-        rerank(
-            topics=topics, corpus=corpus, input_run=runs,
-            reranker_config=vars(args),
-            top_k=args.top_k, batch_size=args.batch_size_per_query,
-            max_length=args.max_length,
-            writer=writer
-        )
-    writer.close()
-
-    print('done')
+    # if interactive:
+    #     return (rerank_results, reranker.get_agent())
+    # else:
+    #     return rerank_results
--- src/wrapper/testing.py
+++ src/wrapper/testing.py
@@ -1,15 +1,15 @@
-
-# index_dir='/home/dju/indexes/peS2o/s2orc-v2.document.lucene'
-# corpus_dir='/home/dju/datasets/peS2o/'
 index_dir='/home/dju/indexes/litsearch/bm25.litsearch.full_documents.lucene/'
 corpus_dir='/home/dju/datasets/litsearch/full_paper/corpus.jsonl'
-example_topic = {'1': 'Are there any research papers on methods to compress large-scale language models using task-agnostic knowledge distillation techniques?'}
-# golden doc: 202719327
+
+from tools.ranking_utils import load_corpus
+corpus = load_corpus(corpus_dir)
+example_topic = {
+    '1': 'Are there any research papers on methods to compress large-scale language models using task-agnostic knowledge distillation techniques?',
+    '2': 'Are there any research papers on new language model pre-training that can outperform masked language modeling?'
+}
+
 writer = None
 
-""" I. First-stage Retrieval
-## [TODO] query reformulation 
-"""
 from retrieve.bm25 import search
 output_run = search(
     index=index_dir,
@@ -17,53 +17,67 @@ output_run = search(
     b=0.4,
     topics=example_topic,
     batch_size=4,
-    k=1000, 
+    k=100, 
     writer=writer
 )
-
-""" II. Retrieval Augmentation 
-## [TODO] Add listwise (mulitdocument) reranking/summarization
-"""
-from tools.ranking_utils import load_corpus
-corpus = load_corpus(corpus_dir)
+# print(output_run)
 
 ## II(a). Passage reranking
-from augment.pointwise import rerank
+# from augment.pointwise import rerank
+# output_run = rerank(
+#     topics=example_topic,
+#     corpus=corpus,
+#     runs=output_run,
+#     reranker_config={
+#         "reranker_class": 'monobert',
+#         "reranker_name_or_path": 'cross-encoder/ms-marco-MiniLM-L-6-v2',
+#         "device": 'cuda',
+#         "fp16": True
+#     },
+#     top_k=1000,
+#     batch_size=2,
+#     max_length=512,
+#     writer=writer,
+# )
+
+## II(b). Passage summariation
+# from augment.pointwise import summarize
+# output_context = summarize(
+#     topics=example_topic,
+#     corpus=corpus,
+#     runs=output_run,
+#     summarizer_config={
+#         "summarizer_class": 'seq2seq',
+#         "summarizer_name_or_path": 'google/flan-t5-base',
+#         'fp16': True,
+#         'flash_attention_2': False
+#     },
+#     top_k=10,
+#     batch_size=2,
+#     max_length=1024,
+#     template="Summarize the document based on the query. Query: {q} Document: {d} Summary: ",
+#     writer=writer,
+# )
+
+## II(c). Passage listwise reranking
+from augment.listwise import rerank
 output_run = rerank(
     topics=example_topic,
     corpus=corpus,
     runs=output_run,
-    reranker_config={
-        "reranker_class": 'monobert',
-        "reranker_name_or_path": 'cross-encoder/ms-marco-MiniLM-L-6-v2',
-        "device": 'cuda',
-        "fp16": True
-    },
-    top_k=1000,
-    batch_size=2,
+    model_path='castorini/first_mistral',
+    top_k=100,
     max_length=512,
-    writer=writer,
-)
-
-## II(b). Passage summariation
-from augment.pointwise import summarize
-output_context = summarize(
-    topics=example_topic,
-    corpus=corpus,
-    runs=output_run,
-    summarizer_config={
-        "summarizer_class": 'seq2seq',
-        "summarizer_name_or_path": 'google/flan-t5-base',
-        'fp16': True,
-        'flash_attention_2': False
-    },
-    top_k=10,
     batch_size=2,
-    max_length=1024,
-    template="Summarize the document based on the query. Query: {q} Document: {d} Summary: ",
-    writer=writer,
+    prompt_mode='rank_GPT',
+    cpntext_size=4096,
+    variable_passages=True,
+    use_logits=False,
+    use_alpha=True,
+    vllm_batched=True,
+    num_gpus=1
 )
-print(output_context)
+print(output_run)
 
 
 """ III. Generation
